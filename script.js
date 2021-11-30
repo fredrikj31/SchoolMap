97fe81794927c111df4e39a93dfd450cbc65d4bd
Fredrik Johansen <fredrik1206@gmail.com>
2021-11-30 18:49:42 +0100

    Added colors

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ $(function () {
 		series: {
 			regions: [
 				{
-					scale: ["#C8EEFF", "#0071A4"],
-					normalizeFunction: "polynomial",
+					values: {
+						"C2.12": "SPORTS & OUTDOOR",
+					},
+					scale: {
+						"FASHION": "#2761ad",
+						"LINGERIE & UNDERWEAR": "#d58aa3",
+						"BEAUTY & SPA": "#ee549f",
+						"URBAN FAVORITES": "#15bbba",
+						"SPORTS & OUTDOOR": "#8864ab",
+						"KIDS STUFF": "#ef4e36",
+						"ENTERTAINMENT": "#e47325",
+						"HOME DECOR": "#a2614f",
+						"LIFESTYLE": "#8a8934",
+						"DINING": "#73bb43",
+						"REST ROOMS": "#6c260f",
+						"SERVICES": "#504d7c",
+						"OTHER": "#c7b789",
+					},
 				},
 			],
 		},
