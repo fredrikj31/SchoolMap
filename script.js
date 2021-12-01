fb37c909a15c88d8fd2d3e58fac8444f27d30985
Fredrik Johansen <fredrik1206@gmail.com>
2021-12-01 14:32:56 +0100

    Added labels to map

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var nums = [1,2,3,4,5,6,7,8,9,10];
+
 $(function () {
 	$("#map").vectorMap({
 		map: "secondFloor",
@@ -28,7 +30,8 @@ $(function () {
 		labels: {
 			regions: {
 				render: function (code) {
-					return regions[code] && jvm.Map.maps[map].paths[code].name;
+					console.log(code);
+					return code;
 				},
 			},
 		},
